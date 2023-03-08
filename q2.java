ff185e0f29f7471a8c2d012746eb60dbcffabb0a
alaa20003 <127278965+alaa20003@users.noreply.github.com>
2023-03-08 23:52:58 +0300

    Create q2.java

---
+++ q2.java
@@ -0,0 +1,12 @@
+public class Q1 {
+    public static void main(String[] args) {
+        circularlyLinkedList<Integer> list = new circularlyLinkedList<>();
+
+        list.addFirst(11);
+        list.addLast(12);
+        list.addLast(13);
+        list.addLast(14);
+        list.addLast(15);
+        list.print();
+    }
+}
